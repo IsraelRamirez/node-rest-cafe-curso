6213f7a51580c6f394f0145f1d4b457b5d560741
Israel Ramirez <israel.ramirezc@utem.cl>
2020-08-16 21:40:22 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -5,4 +5,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 if (process.env.NODE_ENV === 'dev')
     process.env.MONGODB = "mongodb://localhost:27017/cafe"
 else
-    process.env.MONGODB = "mongodb+srv://maximus:yiTQmU30jHIUI6E4@test-cluster-cursonode.myi26.mongodb.net/cafe"
+    process.env.MONGODB = process.env.MONGO_URI
