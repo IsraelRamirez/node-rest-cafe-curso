2eb50f13c9188024b5f465b925714c757ad8d5f6
Israel Ramirez <israel.ramirezc@utem.cl>
2020-08-16 21:50:35 -0400

    Error con heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.HOST = process.env.HOST || "0.0.0.0"
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
-
 if (process.env.NODE_ENV === 'dev')
     process.env.MONGODB = "mongodb://localhost:27017/cafe"
 else
